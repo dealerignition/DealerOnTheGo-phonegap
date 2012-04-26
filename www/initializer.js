1127f48e4be0de52667f2ab8800af353b3ae0cdb
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-04-26 16:07:27 -0400

    Various cleanup of files.

--- www/initializer.js
+++
@@ -1,8 +0,0 @@
-var initialized = false;
-
-$(document).bind("pageinit", function() {
-  if (!initialized) {
-    $.mobile.changePage("http://dealeronthego.herokuapp.com");
-    initialized = true;
-  }
-});
